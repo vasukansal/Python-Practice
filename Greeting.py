c1110c630fab3eeb6951e4426c57a82b6b97a1d1
Vasu Kansal <vasu12b8075@gmail.com>
2023-01-08 16:18:29 +0530

    Real time Greeting

---
+++ Greeting.py
@@ -0,0 +1,16 @@
+import time
+hours=int(time.strftime("%H"))
+minute=int(time.strftime("%M"))
+seconds=int(time.strftime("%S"))
+# print(hours+" "+minute+" "+seconds)
+if(4<=hours<=11 and minute<=59 and seconds<=59):
+    print("Good Morning!! :)")
+
+elif(12<=hours<=16 and minute<=59 and seconds<=59):
+    print("Good Afternoon!! :)")
+
+elif(17<=hours<=21 and minutes<=59 and seconds<=59):
+    print("Goog Evening!! :)")
+
+elif(22<=hours<=3 and minutes<=59 and seconds<=59):
+    print("Good Noght!! :)")

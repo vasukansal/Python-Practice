af5a9adab01a7d4ee5c7d9dcab92884ee4ed56ea
Vasu Kansal <vasu12b8075@gmail.com>
2023-01-10 00:11:23 +0530

    Real time greeting

--- Greeting.py
+++ Greeting.py
@@ -12,5 +12,5 @@ elif(12<=hours<=16 and minute<=59 and seconds<=59):
 elif(17<=hours<=21 and minute<=59 and seconds<=59):
     print("Good Evening!! :)")
 
-elif(22<=hours<=3 and minute<=59 and seconds<=59):
+elif(22<=hours<=23 or 0<=hours<=3 and minute<=59 and seconds<=59):
     print("Good Noght!! :)")

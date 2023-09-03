2efe371e7aa4f8b3bfaa1614d7ea39f98e90ff69
vasukansal <vasu12b8075@gmail.com>
2023-09-03 11:22:45 +0530

    Learning Sockets in python

--- Sockets/clients.py
+++ Sockets/clients.py
@@ -1,6 +1,6 @@
 import socket
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect(('10.4.2.147',55555))
+s.connect(('10.5.3.147',55555))
 msg=s.recv(1024)
 s.close()
-print(msg)
+print(msg.decode())
